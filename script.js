29c757351d229cef0ebb6826593852c2e6b2d4fe
Laxman Ghimire <ghimirelaxman2155@gmail.com>
2025-11-26 22:24:56 +0545

    videos added

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const reelsData = [
         shareCount: 92,
         isFollowed: false,
         caption: "Dark mode > light mode. Change my mind.",
-        video: "./reels/video1.mp4",
+        video: "./videos/tailwinderDog.mp4",
         userprofile: "https://images.unsplash.com/photo-1613915617430-8ab0fd7c6baf?q=80&w=930&auto=format&fit=crop"
     },
 
@@ -19,7 +19,7 @@ const reelsData = [
         shareCount: 65,
         isFollowed: false,
         caption: "Sunsets hit differently when you're far from home 🌅",
-        video: "./reels/video2.mp4",
+        video: "https://player.vimeo.com/external/403787754.sd.mp4?s=751ee292a7d12075b1219777084ff57ac4fe8f6a&profile_id=139&oauth2_token_id=57447761",
         userprofile: "https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?q=80&w=930&auto=format&fit=crop"
     },
 
@@ -31,7 +31,7 @@ const reelsData = [
         shareCount: 144,
         isFollowed: false,
         caption: "No excuses. Only results. 💪",
-        video: "./reels/video3.mp4",
+        video: "https://player.vimeo.com/external/389953115.sd.mp4?s=086abca33ccce1ce01a01694d94be87cb528b04f&profile_id=139&oauth2_token_id=57447761",
         userprofile: "https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e?q=80&w=930&auto=format&fit=crop"
     },
 
@@ -43,7 +43,7 @@ const reelsData = [
         shareCount: 70,
         isFollowed: false,
         caption: "If eating is a hobby, then I’m a professional 😌✨",
-        video: "./reels/video4.mp4",
+        video: "https://player.vimeo.com/external/209220772.sd.mp4?s=b628266cb14afb3e8ceea1f63eb694a7f2a7bc01&profile_id=164&oauth2_token_id=57447761",
         userprofile: "https://images.unsplash.com/photo-1527980965255-d3b416303d12?q=80&w=930&auto=format&fit=crop"
     },
 
@@ -55,7 +55,7 @@ const reelsData = [
         shareCount: 110,
         isFollowed: false,
         caption: "City lights > everything else.",
-        video: "./reels/video5.mp4",
+        video: "https://player.vimeo.com/external/331511974.sd.mp4?s=d545f2c051f7ed3d6fbaf0a2f03c2d796ff51697&profile_id=139&oauth2_token_id=57447761",
         userprofile: "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?q=80&w=930&auto=format&fit=crop"
     },
 
@@ -67,7 +67,7 @@ const reelsData = [
         shareCount: 220,
         isFollowed: false,
         caption: "Just a dog making your day better 🐶💛",
-        video: "./reels/video6.mp4",
+        video: "https://player.vimeo.com/external/371623630.sd.mp4?s=608646f26d2f0cb9d989cd803cc4ed2cf81cd7c7&profile_id=139&oauth2_token_id=57447761",
         userprofile: "https://images.unsplash.com/photo-1544005313-94ddf0286df2?q=80&w=930&auto=format&fit=crop"
     },
 
@@ -79,7 +79,7 @@ const reelsData = [
         shareCount: 75,
         isFollowed: false,
         caption: "This gadget changed everything.",
-        video: "./reels/video7.mp4",
+        video: "https://player.vimeo.com/external/310453586.sd.mp4?s=36fdf6b2fda5270641edcfc7e9ccb2fdbef674e7&profile_id=139&oauth2_token_id=57447761",
         userprofile: "https://images.unsplash.com/photo-1544723795-3fb6469f5b39?q=80&w=930&auto=format&fit=crop"
     },
 
@@ -91,17 +91,56 @@ const reelsData = [
         shareCount: 150,
         isFollowed: false,
         caption: "Life is better on two wheels 🏍️🔥",
-        video: "./reels/video8.mp4",
+        video: "https://player.vimeo.com/external/310438002.sd.mp4?s=1a01444760c63375783a7ab7c4b2ba4f7ced2c14&profile_id=139&oauth2_token_id=57447761",
         userprofile: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?q=80&w=930&auto=format&fit=crop"
     }
 ];
 
 
 
+
 var sum = '';
 
-reelsData.forEach(()=>{
-    console.log("hii")
+reelsData.forEach((elem)=>{
+    sum = sum + `<div class="reel">
+            <video src=${elem.video} autoplay loop muted></video>
+            <div class="bottom">
+              <div class="user">
+                <img src=${elem.userprofile} alt="" />
+                <h4>${elem.username}</h4>
+                <button>${elem.isFollowed}</button>
+              </div>
+              <h3>
+                ${elem.caption}
+              </h3>
+            </div>
+            <div class="right">
+              <div class="like">
+                <h4 class="like-icon"><i class="ri-heart-line"></i></h4>
+                <h6>${elem.likeCount}</h6>
+              </div>
+
+              <div class="comment">
+                <h4 class="comment-icon"><i class="ri-chat-3-line"></i></h4>
+                <h6>${elem.commentCount}</h6>
+              </div>
+
+              <div class="share">
+                <h4 class="share-icon"><i class="ri-send-plane-line"></i></h4>
+                 <h6>${elem.shareCount}</h6>
+              </div>
+
+              <div class="save">
+                <h4 class="save-icon"><i class="ri-bookmark-line"></i></h4>
+              </div>
+
+              <div class="dot">
+                <h4 class="dot-icon"><i class="ri-more-2-line"></i></h4>
+              </div>
+            </div>
+          </div>`
 })
 
 var allReels = document.querySelector(".all-reels");
+
+allReels.innerHTML = sum
